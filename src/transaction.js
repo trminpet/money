b3f96c8881f8d86b7217cfcff77c26a6d9ba1053
Štefan Prokop <sprokop1010@seznam.cz>
2019-04-13 18:51:23 +0200

    Add transaction & user object structure

---
+++ src/transaction.js
@@ -0,0 +1,15 @@
+
+// Transaction (* means required, fields without star are optional)
+//      - * name
+//      - * price
+//      - * code
+//      - * from (object of type User)
+//      - * to (object of type User)
+//      - * state (enum - incoming, outcoming)
+//      - * createdAt
+//      - description
+
+// User
+//      - * name
+//      - * surname
+//      - * bankAccount
